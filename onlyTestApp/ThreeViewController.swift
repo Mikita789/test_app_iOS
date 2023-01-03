da68623291817457ed9a1f07106e8ef88f48080e
Mikita <nikita_popow_1993@mail.ru>
2023-01-03 10:08:35 +0300

    threeViewController active constraints

--- onlyTestApp/ThreeViewController.swift
+++ onlyTestApp/ThreeViewController.swift
@@ -28,10 +28,11 @@ class ThreeViewController: UIViewController {
         self.imageView = UIImageView(image: startIm)
         self.imageView.center.x = self.view.center.x
         self.imageView.contentMode = .scaleAspectFit
+        self.imageView.layer.borderWidth = 1
         self.view.addSubview(imageView)
         self.imageView.translatesAutoresizingMaskIntoConstraints = false
         self.imageView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        self.imageView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -10).isActive = true
+        self.imageView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -view.bounds.height*0.03).isActive = true
         self.imageView.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 1).isActive = true
         self.imageView.topAnchor.constraint(equalTo: view.topAnchor, constant: view.bounds.height*0.45).isActive = true
         
@@ -46,8 +47,8 @@ class ThreeViewController: UIViewController {
         self.datePicker.addTarget(self, action: #selector(datePickAction), for: .valueChanged)
         self.view.addSubview(datePicker)
         self.datePicker.translatesAutoresizingMaskIntoConstraints = false
-        self.datePicker.topAnchor.constraint(equalTo: view.topAnchor, constant: 90).isActive = true
-        self.datePicker.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -10).isActive = true
+        self.datePicker.topAnchor.constraint(equalTo: view.topAnchor, constant: view.bounds.height*0.1).isActive = true
+        self.datePicker.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -view.bounds.width*0.03).isActive = true
         self.datePicker.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.4).isActive = true
         self.datePicker.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.06).isActive = true
         
@@ -57,13 +58,12 @@ class ThreeViewController: UIViewController {
         self.infoLabel.numberOfLines = .max
         self.infoLabel.textAlignment = .justified
         self.infoLabel.contentMode = .scaleAspectFit
-        //self.infoLabel.layer.borderWidth = 2
-        
         self.infoLabel.minimumScaleFactor = 0.4
+        self.infoLabel.layer.borderWidth = 1
         self.infoLabel.adjustsFontSizeToFitWidth = true
         self.view.addSubview(infoLabel)
         self.infoLabel.translatesAutoresizingMaskIntoConstraints = false
-        self.infoLabel.topAnchor.constraint(equalTo: datePicker.bottomAnchor, constant: 10).isActive = true
+        self.infoLabel.topAnchor.constraint(equalTo: datePicker.bottomAnchor, constant: view.bounds.height*0.01).isActive = true
         self.infoLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
         self.infoLabel.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
         self.infoLabel.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.25).isActive = true
@@ -75,7 +75,7 @@ class ThreeViewController: UIViewController {
         self.selectDateLabel.textAlignment = .right
         self.view.addSubview(selectDateLabel)
         self.selectDateLabel.translatesAutoresizingMaskIntoConstraints = false
-        self.selectDateLabel.rightAnchor.constraint(equalTo: datePicker.leftAnchor, constant: -10).isActive = true
+        self.selectDateLabel.rightAnchor.constraint(equalTo: datePicker.leftAnchor, constant: -view.bounds.width*0.01).isActive = true
         self.selectDateLabel.centerYAnchor.constraint(equalTo: datePicker.centerYAnchor, constant: 0).isActive = true
         self.selectDateLabel.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.3).isActive = true
         self.selectDateLabel.heightAnchor.constraint(equalToConstant: datePicker.bounds.height).isActive = true
@@ -88,7 +88,7 @@ class ThreeViewController: UIViewController {
         self.urlImageTexrView.textAlignment = .center
         self.view.addSubview(urlImageTexrView)
         self.urlImageTexrView.translatesAutoresizingMaskIntoConstraints = false
-        self.urlImageTexrView.topAnchor.constraint(equalTo: infoLabel.bottomAnchor, constant: 20).isActive = true
+        self.urlImageTexrView.topAnchor.constraint(equalTo: infoLabel.bottomAnchor, constant: view.bounds.height*0.005).isActive = true
         self.urlImageTexrView.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: 0).isActive = true
         self.urlImageTexrView.leftAnchor.constraint(equalTo: view.leftAnchor, constant: self.view.bounds.width*0.08).isActive = true
         self.urlImageTexrView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.025).isActive = true
